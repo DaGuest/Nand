8ea0be5bb7e0bc64cf609aa0148dcb689b8aad69
Daan Gast <daangast@gmail.com>
2024-08-27 18:11:18 +0200

    Added documentation

--- src/main/cpp/vmtranslator.h
+++ src/main/cpp/vmtranslator.h
@@ -7,7 +7,14 @@ class VMTranslator
 {
 public:
     VMTranslator();
+    /**
+     * Intitializes a parser and codeWriter object.
+     */
     VMTranslator(std::string outputFileName);
+    /**
+     * Advances the parser until the EOF.
+     * Separates Arithmetic, Pop and Push commands from each other.
+     */
     void start();
 
 private:
