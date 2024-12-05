0176728e77bd5a355e3d30c3d5ddd8a255859263
Daan Gast <daangast@gmail.com>
2024-12-05 20:10:39 +0100

    Added an implementation to write tokens into output file

--- src/main/haskell/JackAnalyzer.hs
+++ src/main/haskell/JackAnalyzer.hs
@@ -1,14 +1,37 @@
+import Control.Monad (filterM)
 import JackTokenizer
+import System.Directory
 import System.Environment (getArgs)
+import System.FilePath
 import System.IO
 
 main = do
   (args : _) <- getArgs
-  contents <- readFile args
-  print $ tokenize contents
+  files <- fileNames args
+  fileContents <- mapM readFile files
+  let fileNameWithTokens = zip files $ map tokenize fileContents
+  mapM (\(f, t) -> writeFile "test.txt" (concat (map showTokens t))) fileNameWithTokens
 
 -- TODO
 
--- A function to enumerate a folder and get all .jack files
+-- Helper function that checks if the given argument is a folder
+isFolder :: FilePath -> Bool
+isFolder arg = takeExtension arg == ""
+
+-- Enumerate a folder and get all .jack files
+getFiles :: FilePath -> IO [FilePath]
+getFiles dir = filterM (\n -> return $ takeExtension n == ".jack") =<< getDirectoryContents dir
+
+-- Iterate through all the filenames
+fileNames :: FilePath -> IO [FilePath]
+fileNames dir
+  | isFolder dir = getFiles dir
+  | otherwise = return [dir]
 
 -- A function to compile all the .jack files into the .xml output file
+showTokens :: Token -> String
+showTokens (TokIdent s) = "<identifier>" ++ s ++ "</identifier>\n"
+showTokens (TokSymbol c) = "<symbol>" ++ [c] ++ "</symbol>\n"
+showTokens (TokKey s) = "<keyword>" ++ s ++ "</keyword>\n"
+showTokens (TokInt i) = "<keyword>" ++ "</keyword>\n"
+showTokens (TokStr s) = "<string>" ++ s ++ "</string>\n"
